19d00ea5a69b1681991691104c2ac3d9878e4a3a
Devin <devin@devin.dev>
2025-03-06 04:57:15 +0000

    Update servlet imports to use jakarta namespace

--- src/main/java/com/hazcom/sso/security/AuthenticationExceptionHandler.java
+++ src/main/java/com/hazcom/sso/security/AuthenticationExceptionHandler.java
@@ -2,8 +2,8 @@ package com.hazcom.sso.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthenticationExceptionHandler implements AuthenticationFailureHandler {
